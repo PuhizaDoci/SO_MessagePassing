fdd9cf01f01975b4588af1594160fb524a2791ca
PuhizaDoci <puhize.doci@studentet.uni-pr.edu>
2020-01-08 18:11:00 +0100

    Klasat Klient/Server
    Co-Authored-By: rrezartasallauka <rrezartasallauka@users.noreply.github.com>
    Co-Authored-By: medinakrelani <medinakrelani@users.noreply.github.com>
    Co-Authored-By: hajrijemjeku <hajrijemjeku@users.noreply.github.com>

---
+++ SistemeOperative/SistemeOperative/Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.Mail;
+using Newtonsoft.Json;
+
+namespace SistemeOperative
+{
+    class Server
+    {
+        //_______________________Funksioni Main, punuar nga Medina Krelani_______________
+        static void Main(string[] args)
+        {
+            //Krijohet një instancë e klasës IPEndPoint e cila merr një IP adresë dhe një port për koneksion
+            IPEndPoint ep = new IPEndPoint(IPAddress.Loopback, 1234);
+            //Serveri fillon të dëgjojë
+            TcpListener listener = new TcpListener(ep);
+            listener.Start();
+
+            Console.WriteLine(@"  
+            ===================================================  
+                   Serveri po dëgjon në: {0} Porti :{1}  
+            ===================================================",
+            ep.Address, ep.Port);
+
+            // Pjesa e serverit. Unaza përsëritet pa u ndërprerë, dmth serveri rrin duke dëgjuar në portin 1234
+            while (true)
+            {
+                const int bytesize = 1024 * 1024;
+
+                string message = null;
+                byte[] buffer = new byte[bytesize];
+
+                //Pranohet kërkesa e Klientit për lidhje me server
+                var sender = listener.AcceptTcpClient();
+                //Lexon të dhënat e ardhura nga Klienti dhe i ruan ato në një Byte Array
+                sender.GetStream().Read(buffer, 0, bytesize);
+
+                message = cleanMessage(buffer);
+
+                // Ruan të dhënat e ardhura nga Klienti  
+                Person person = JsonConvert.DeserializeObject<Person>(message); // Deserializohet mesazhi 
+                Console.WriteLine("Mesazh nga: " + person.Name);
+
+                byte[] bytes = System.Text.Encoding.Unicode.GetBytes("Faleminderit që dërguat mesazh, " + person.Name);
+                sender.GetStream().Write(bytes, 0, bytes.Length); // Dërgon pergjigjen 
+
+                //thirret funksioni sendEmail i inicializuar më poshtë
+                sendEmail(person);
+            }
+        }
+        /* Funksioni sendEmail merr të dhënat e ardhura nga Klienti dhe i përdor ato për dërgimin e një email*/
+        //__________________SendEmail dhe CleanMessage, punuar nga Puhizë Doçi______________________
+        private static void sendEmail(Person p)
+        {
+            try
+            {
+
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com", 465))
+                {
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential("rrezartasallauka@gmail.com", "049552503rr.");
+                    // Email dergohet
+                    client.Send(
+                        new MailMessage("rrezartasallauka@gmail.com", p.Email,
+                            "Programi jonë me WebService!",
+                            string.Format(@"Faleminderit që përdorët programin tonë, {0}.  
+                            Mesazhi është marrë me sukses, '{1}'.", p.Name, p.Message
+                            )
+                        )
+                    );
+                }
+
+                Console.WriteLine("Email u dërgua në: " + p.Email);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Email nuk ka mundur të dërgohet!");
+            }
+        }
+
+        private static string cleanMessage(byte[] bytes)
+        {
+            string message = System.Text.Encoding.Unicode.GetString(bytes);
+
+            string messageToPrint = null;
+            foreach (var nullChar in message)
+            {
+                if (nullChar != '\0')
+                {
+                    messageToPrint += nullChar;
+                }
+            }
+            return messageToPrint;
+        }
+
+        // Sends the message string using the bytes provided.  
+        //___________________Punuar Puhizë Doçi__________________
+        private static void sendMessage(byte[] bytes, TcpClient client)
+        {
+            client.GetStream()
+                .Write(bytes, 0,
+                bytes.Length); // Send the stream  
+        }
+
+    }
+    //________Klasa e punuar nga Rrezarta Sallauka______________________
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+
+
+
+}
---
+++ SistemeOperative/SistemeOperativeKlient/Klient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.Mail;
+using Newtonsoft.Json;
+using SistemeOperative;
+
+namespace SistemeOperativeKlient
+{
+    public class Klient
+    {
+        // _____________Funksioni Main punuar nga Rrezarta Sallauka_____________
+        static void Main(string[] args)
+        {
+            //Krijon një instancë të klasës Person
+            Person person = new Person();
+            //Kërkon nga useri të shkruajë të dhënat që shkojnë si parametra të klasës Person
+            Console.Write("Enter your name: ");
+            person.Name = Console.ReadLine();
+            Console.Write("Enter your email address: ");
+            person.Email = Console.ReadLine();
+            Console.Write("Enter your message: ");
+            person.Message = Console.ReadLine();
+
+            /*Të dhënat në objektin "person" serializohen, enkodohen në bajta dhe me anë të funksionit sendMessage (i krijuar më poshtë)
+             këto bajta dërgohen tek serveri*/
+            byte[] bytes = sendMessage(System.Text.Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(person)));
+        }
+
+        //funksioni sendMessage i cili dërgon informatat nga klienti në server,
+        //_________________Punuar nga Hajrije Mjeku______________________________
+        private static byte[] sendMessage(byte[] messageBytes)
+        {
+            const int bytesize = 1024 * 1024;
+
+
+            try // Provon të kryej lidhjen me server dhe t'i dërgojë bajtat
+            {
+
+                System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("127.0.0.1", 1234); // Krijohet një koneksion i ri
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(messageBytes, 0, messageBytes.Length); // Shkruhen bajtat  
+                Console.WriteLine("=============================================");
+                Console.WriteLine("=  Lidhja me server është kryer me sukses   =");
+                Console.WriteLine("=============================================\n");
+
+
+                messageBytes = new byte[bytesize]; // fshihen bajtat
+                // Lexohen bajtat e ardhur nga server
+                
+                stream.Read(messageBytes, 0, messageBytes.Length);
+
+
+                // Mbyllet soketa
+                stream.Dispose();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Shtypni një karakter për të vazhduar!");
+            Console.ReadLine();
+            return messageBytes; // Kthehet pergjigja     
+
+        }
+    }
+
+}
